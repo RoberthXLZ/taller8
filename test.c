82f755cf4e8a4aec97d683b6e7ad45a821330605
marcosan <marcoxavibsc@gmail.com>
2017-06-22 16:39:35 -0500

    archivo final

--- test.c
+++ test.c
@@ -19,8 +19,17 @@ int main(int argc, char *argv[])
 {
 	int fd = -1;
 	int baudrate = 9600;  // default
+	int contador=1;
+	char temp='t', hum='h';
+	char* humedad=&hum;
+	char* temperatura=&temp;
+	char buffer[3];
+	float artemp[12];
+	float arhum[12];
+	float mt;
+	float mh;
 
-	fd = serialport_init("/dev/ttyACM0", baudrate);
+	fd = serialport_init("/dev/ttyACM1", baudrate);
 
 	if( fd==-1 )
 	{
@@ -28,6 +37,33 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	serialport_flush(fd);
+
+	while(1){
+		write(fd,temperatura,1);
+		usleep(5000000);
+		read(fd,buffer,1);
+		artemp[contador]=buffer[0];
+		printf("Temperatura: %d\n",buffer[0]);
+
+		write(fd, humedad, 1);
+		usleep(5000000);
+		read(fd, buffer, 1);
+		arhum[contador]=buffer[0];
+		printf("Humedad : %d\n", buffer[0]);
+
+		if(contador==12){
+			mt=calculateSD(temp);
+			mh=calculateSD(hum);
+
+			printf("Temperatura Media : %.3f y su desviacion es: %.3f\n" ,mt, pow(mt,2));
+			printf("Humedad Media: %.3f y su desviacion es: %.3f\n" ,mh, pow(mh,2));
+			counter=-1;
+		}
+		contador++;
+
+	}
+	
 	
 	close( fd );
 	return 0;	
@@ -53,3 +89,12 @@ float calculateSD(float data[])
     return sqrt(standardDeviation / 10);
 }
 
+/*
+float promedio(float data[]){
+	int cont=0, media=0,suma=0  ;	
+	while(data.lengt){
+		suma=suma+data[i];
+		cont=cont+1;		
+	}	
+	return 
+}*/
